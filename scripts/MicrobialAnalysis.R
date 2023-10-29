af781f84b62e02c974d4b727f406d0758fde6fe1
MartinGenner <64019956+MartinGenner@users.noreply.github.com>
2023-10-29 10:06:08 +0000

    Update MicrobialAnalysis.R

--- scripts/MicrobialAnalysis.R
+++ scripts/MicrobialAnalysis.R
@@ -5,6 +5,8 @@ Bacteria_only <- Bacteria[,c(3:155)]
 Bacteria_depth <- Bacteria[,c(1:2)]
 
 library(vegan)
+library(khroma)
+library(ggplot2)
 
 cca <- cca(Bacteria_only ~ Depth, data=Bacteria_depth)
 summary(cca)
@@ -18,7 +20,7 @@ BacteriaPlotData
 BactPlot <- ggplot(BacteriaPlotData, aes(x=CCA1, y=Depth)) +
   theme_classic() +
   geom_point(size=3) + scale_y_reverse() +
-  geom_smooth(method = "gam", formula = y ~ s(x, bs = "cs", k = 3), size = 1.5, orientation = "y", se = FALSE, span = 2) +
+  geom_smooth(method = "gam", formula = y ~ s(x, bs = "cs", k = 3), size = 1.5, orientation = "y", se = TRUE, span = 2) +
   labs(x = "CCA1", y = "Depth (m)") +
   scale_x_continuous(limits = c(-2, 2), breaks =c(-2,-1,0,1,2)) +
   ylim(25, 0) +  
